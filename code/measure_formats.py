d007d9c00ea472ed567c2c920d212f4aa2ed28ce
k0rsakov <korsakov.iyu@gmail.com>
2024-11-20 11:33:42 +0300

    Изменен проект infrastructure_for_data_engineer_file_formats: - Добавлен скрипт для подсчёта объёма разных форматов

---
+++ code/measure_formats.py
@@ -0,0 +1,35 @@
+import os
+
+import pandas as pd
+
+
+def measure_formats() -> pd.DataFrame:
+    """
+    Функция, которая позволяет посчитать объём файлов в разных форматах.
+
+    :return: pd.DataFrame c данными.
+    """
+
+    results = []
+
+    # Размеры файлов
+    files = {
+        "parquet": "../data/data.parquet",
+        "csv": "../data/data.csv",
+        "csv.gz": "../data/data.csv.gz",
+        "json": "../data/data.json",
+    }
+
+    for format_name, filename in files.items():
+        if os.path.exists(filename):
+            size = os.path.getsize(filename)
+
+            results.append({
+                "format": format_name,
+                "size_mb": size / (1024 * 1024),
+            })
+
+    return pd.DataFrame(results)
+
+
+print(measure_formats())

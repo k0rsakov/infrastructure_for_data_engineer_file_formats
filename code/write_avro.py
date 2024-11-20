c6c0c6a9f989eb4b2f7d3b4b0c4fdd35b2789eae
k0rsakov <korsakov.iyu@gmail.com>
2024-11-20 11:14:50 +0300

    Изменен проект infrastructure_for_data_engineer_file_formats: - Добавлен пример записи .avro

---
+++ code/write_avro.py
@@ -0,0 +1,56 @@
+import json
+
+import avro.schema
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter
+from generate_data import generate_users
+
+df = generate_users(size_of_generate=1)
+
+
+# Определение схемы
+schema = {
+    "type": "record",
+    "name": "user",
+    "fields": [
+        {"name": "id", "type": "string"},
+        {"name": "created_at", "type": "string"},
+        {"name": "updated_at", "type": "string"},
+        {"name": "first_name", "type": "string"},
+        {"name": "middle_name", "type": "string"},
+        {"name": "birthday", "type": "string"},
+        {"name": "email", "type": "string"},
+        {"name": "city", "type": "string"},
+    ],
+}
+# Преобразуем datetime колонки в строки
+datetime_columns = ['created_at', 'updated_at', 'birthday']
+for col in datetime_columns:
+    df[col] = df[col].astype(str)
+# Запись в Avro
+with DataFileWriter(
+        open("../data/data.avro", "wb"),
+        DatumWriter(),
+        avro.schema.parse(json.dumps(schema)),
+) as writer:
+    for _, row in df[[
+            "id",
+            "created_at",
+            "updated_at",
+            "first_name",
+            "last_name",
+            "middle_name",
+            "birthday",
+            "email",
+            "city"]].iterrows():
+        writer.append({
+            "id": row["id"],
+            "created_at": row["created_at"],
+            "updated_at": row["updated_at"],
+            "first_name": row["first_name"],
+            "middle_name": row["middle_name"],
+            "birthday": row["birthday"],
+            "email": row["email"],
+            "city": row["city"],
+        })
+

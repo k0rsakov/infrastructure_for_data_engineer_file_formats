c58152eeba36f8beecabfb183f23a2429d4449aa
k0rsakov <korsakov.iyu@gmail.com>
2024-11-20 05:49:06 +0300

    Изменен проект infrastructure_for_data_engineer_file_formats: - Добавлен записи .csv

--- code/write_csv_gz.py
+++ code/write_csv_gz.py
@@ -3,6 +3,7 @@ from generate_data import generate_users
 df = generate_users()
 
 df.to_csv(
-    path_or_buf="../data/data.csv",
+    path_or_buf="../data/data.csv.gz",
+    compression="gzip",
     index=False,
 )
